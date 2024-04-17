a56af710464cb57a08bff43473da985683f72e49
Frank_Chun_Hing <frank.ho@powercoders.org>
2024-04-17 08:16:46 +0200

    week3 interactive CSS

---
+++ week3/exercise/exercise_5/index.js
@@ -0,0 +1,12 @@
+document.getElementById('hamburger').addEventListener('click', function() {
+    document.querySelector('nav').classList.toggle("new_nav");
+    document.querySelector('.hamburger').classList.toggle("none");
+    document.querySelector('.cross').classList.toggle("new_cross");
+});
+
+    
+document.getElementById('cross').addEventListener('click', function() {
+    document.querySelector('nav').classList.toggle("new_nav");
+    document.querySelector('.hamburger').classList.toggle("none");
+    document.querySelector('.cross').classList.toggle("new_cross");
+});
